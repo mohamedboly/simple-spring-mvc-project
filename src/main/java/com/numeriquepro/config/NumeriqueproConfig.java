156dadbd25f6eb43d94dee5066b90e8cfe8cdc2a
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-06-14 13:30:57 -0400

    spring mvc annotation configuration

--- src/main/java/com/numeriquepro/config/NumeriqueproConfig.java
+++ src/main/java/com/numeriquepro/config/NumeriqueproConfig.java
@@ -1,10 +1,21 @@
 package com.numeriquepro.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan({"com.numeriquepro"})
 public class NumeriqueproConfig {
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		vr.setPrefix("WEB-INF/");// ou se trouve nos page web
+		vr.setSuffix(".jsp"); // extension des fichier c'est ici qu'il faut agir quand on change de template de vue
+		return vr;
+		
+	}
 
 }
--- src/main/java/com/numeriquepro/controller/AddController.java
+++ src/main/java/com/numeriquepro/controller/AddController.java
@@ -2,17 +2,18 @@ package com.numeriquepro.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AddController {
 	
 	@RequestMapping("/add")
-	public ModelAndView add() {
+	public ModelAndView add(@RequestParam("prenom") String prenom, @RequestParam("nom") String nom) {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("WEB-INF/welcome.jsp");
-		mv.addObject("prenom", "Mohamadou");
-		mv.addObject("nom", "Boly");
+		mv.setViewName("welcome"); // on indique ici seulemet le nom de la page
+		mv.addObject("prenom", prenom);
+		mv.addObject("nom", nom);
 		return  mv;
 		
 	}
